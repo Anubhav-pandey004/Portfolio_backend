a029024e8ad823951358bc5a9c8b0395dde72c77
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-09-15 21:14:14 +0530

    Update routes.js

--- router/routes.js
+++ router/routes.js
@@ -4,5 +4,8 @@ const router =express.Router()
 
 router
     .route("/sendEmail")
-    .post(sendEmail)    
+    .post(sendEmail)  
+router
+    .route("/")
+    .get(res.send("Welcome to my portfolio follow the link https://anubhavpandey.netlify.app/ "))
 module.exports=router 
